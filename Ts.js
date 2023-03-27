0c7d55ebb8090d6b0f3185df1c92510eab1af586
LadyAudrey <theaudreygreene@gmail.com>
2023-03-26 17:51:06 -0700

    first commit

---
+++ Ts.js
@@ -0,0 +1,415 @@
+// Js for backend of album info page on Taylor Swift Tribute Site
+// HardCoding Album Info
+
+// explanation for re-recording dates- when releaed, update in the album, not here
+const unknown =
+  "Taylor hasn't re-released her album yet! So this Taylor's Version does not have cover art yet, but if you click on the album immediately above it, you can see the info from the original album.";
+
+const debut = {
+  trackList: [
+    "TimMcGraw",
+    "Picture to Burn",
+    "Teardrops On My guitar",
+    "A Place in this World",
+    "Cold as You",
+    "The Outside",
+    "Tied Together with a Smile",
+    "Stay Beautiful",
+    "Should've Said No",
+    "Mary's Song",
+    "OurSong",
+    "I'm only Me when I'm with You",
+    "Invisible",
+    "A Perfectly Good Heart",
+  ],
+  released: ["October 24, 2006", unknown],
+  quotes: [
+    "You put up the walls and paint them all a shade of gray\n  And I stood there loving you, and wished them all away \n  And you come away with a great little story \n  Of a mess of a dreamer with the nerve to adore you",
+  ],
+};
+
+const fearless = {
+  trackList: [
+    "Fearless",
+    "Fifteen",
+    "Love Story",
+    "Hey Stephen",
+    "White Horse",
+    "You being with Me",
+    "Breathe",
+    "Tell Me Why",
+    "You're not Sorry",
+    "The Way I loved You",
+    "Forever and Always",
+    "The Best Day",
+    "Change",
+    "Jump then Fall",
+    "Untouchable",
+    "Come in with the Rain",
+    "Superstar",
+    "The other side of the Door",
+    "Today was a Fairytale",
+    "You all over Me",
+    "Mr. Perfectly Fine",
+    "We were Happy",
+    "That's When",
+    "Don't You",
+    "Bye Bye Baby",
+  ],
+  released: ["November 11, 2008", "April 9, 2021"],
+  quotes: [
+    "Back then I swore I was gonna marry him some day\nBut I realized some bigger dreams of mine",
+    "I miss screaming and fighting and kissing in the rain/  It's 2 a.m. and I'm cursing your name\n  I'm so in love that I acted insane\n  And that's the way I loved you\n  Breaking down and coming undone\n  It's a roller coaster kind of rush\n  And I never knew I could feel that much\n  And that's the way I loved you",
+  ],
+};
+
+const speakNow = {
+  trackList: [
+    "Mine",
+    "Sparks Fly",
+    "Back to December",
+    "Speak Now",
+    "Dear John",
+    "Mean",
+    "The Story of Us",
+    "Never Grow Up",
+    "Enchanted",
+    "Better than Revenge",
+    "Innocent",
+    "Haunted",
+    "Last Kiss",
+    "Long Live",
+    "Ours",
+    "If this was a Movie",
+    "Superman",
+  ],
+  released: ["October 25, 2010", unknown],
+  quotes: [
+    "Your guard is up and I know why/Because the last time you saw me is still burned in the back of your mind/You gave me roses and I left them there to die",
+    "You, with your switching sides and your wildfire lies and your humiliation/You have pointed out my flaws again, as if I don't already see them",
+  ],
+};
+
+const red = {
+  trackList: [
+    "State of Grace",
+    "Red",
+    "Treacherous",
+    "I knew you were Trouble",
+    "All Too Well",
+    "22",
+    "I almost Do",
+    "We are Never Ever Getting Back Together",
+    "Stay Stay Stay",
+    "The Last Time",
+    "Holy Ground",
+    "Sad Beautiful Tragic",
+    "The Lucky One",
+    "Everything has Changed",
+    "Starlight",
+    "Begin Again",
+    "The Moment I Knew",
+    "Come Back... Be Here",
+    "Girl at Home",
+    "State of Grace",
+    "Ronan",
+    "Better Man",
+    "Nothing New",
+    "Babe",
+    "Message in a Bottle",
+    "Forever Winter",
+    "Run",
+    "The Very First Night",
+  ],
+  released: ["October 22, 2012", unknown],
+  quotes: [
+    "Loving him is like driving a new Maserati down a dead end street\nFaster than the wind, passionate as sin, ending so suddenly\n  Loving him is like trying to change your mind\n  Once you're already flying through the free fall\n  Like the colors in autumn, so bright, just before they lose it all",
+    "Put your lips close to mine\n  As long as they don't touch\n  Out of focus, eye to eye\n  'Til the gravity's too much\n  And I'll do anything you say\n  If you say it with your hands\n  And I'd be smart to walk away\n  But you're quicksand",
+  ],
+};
+
+const nineteen89 = {
+  trackList: [
+    "Welcome to New York",
+    "Blank Space",
+    "Out of the Woods",
+    "All You had to do was Stay",
+    "Shake It Off",
+    "I Wish You Would",
+    "Bad Blood",
+    "Wildest Dreams",
+    "How You Get the Girl",
+    "This Love",
+    "I Know Places",
+    "Clean",
+    "Wonderland",
+    "You are in Love",
+    "New Romantics",
+  ],
+  released: [
+    "October 27, 2014",
+    "This love and Wildest Dreams have been released; the rest of the albums' release date is unknown",
+  ],
+  quotes: [
+    "You took a Polaroid of us\n  Then discovered\n  The rest of the world was black and white\n  But we were in screaming color",
+    "You two are dancing in a snow globe, 'round and 'round\nAnd he keeps the picture of you in his office downtown\nAnd you understand now why they lost their minds and fought the wars\nAnd why I've spent my whole life tryin' to put it into words",
+  ],
+};
+
+const reputation = {
+  trackList: [
+    "Ready for It",
+    "End Game",
+    "I Did Something Bad",
+    "Don't Blame Me",
+    "Delicate",
+    "Look What You Made Me Do",
+    "So It Goes",
+    "Gorgeous",
+    "Getaway Car",
+    "King of My Heart",
+    "Dancing with Our Hands Tied",
+    "Dress",
+    "This is Why We Can't Have Nice Things",
+    "Call It What You Want",
+    "New Year's Day",
+  ],
+  released: ["November 10, 2017", unknown],
+  quotes: [
+    "You should take it as a compliment\nThat I got drunk and made fun of the way you talk\nYou should think about the consequence\nOf your magnetic field being a little too strong",
+    "I want your midnights\n  But I'll be cleaning up bottles with you on New Year's Day",
+    "You're so gorgeous\nI can't say anything to your face\n'Cause look at your face Gorgeous\nAnd I'm so furious\nAt you for making me feel this way\nBut what can I say\nYou're gorgeous",
+  ],
+};
+
+const lover = {
+  trackList: [
+    "I Forgot that You Existed",
+    "Cruel Summer",
+    "Lover",
+    "The Man",
+    "The Archer",
+    "I Think He Knows",
+    "Miss Americana & the Heartbreak Prince",
+    "Paper Rings",
+    "Cornelia Street",
+    "Death by a Thousand Cuts",
+    "London Boy",
+    "Soon You'll Get Better",
+    "False God",
+    "You Need to Calm Down",
+    "Afterglow",
+    "ME!",
+    "It's Nice to Have a Friend",
+    "Daylight",
+  ],
+  released: "August 23, 2019",
+  quotes: ["It isn’t love it isn’t hate it’s just indifference"],
+};
+
+const folklore = {
+  trackList: [
+    "the 1",
+    "cardigan",
+    "the last great american dynasty",
+    "exile",
+    "my tears ricochet",
+    "mirrorball",
+    "seven",
+    "august",
+    "this is me trying",
+    "illicit affairs",
+    "invisible string",
+    "mad woman",
+    "epiphany",
+    "betty",
+    "peace",
+    "hoax",
+    "the lakes",
+  ],
+  released: "August 18, 2020",
+  quotes: [],
+};
+
+const evermore = {
+  trackList: [
+    "willow",
+    "champagne problems",
+    "gold rush",
+    "'tis the damn season",
+    "tolerate it",
+    "no body, no crime",
+    "happiness",
+    "dorothea",
+    "coney island",
+    "ivy",
+    "cowboy like me",
+    "long story short",
+    "marjorie",
+    "closure",
+    "evermore",
+    "right where you left me",
+    "it's time to go",
+  ],
+  released: "December 18, 2020",
+  quotes: [
+    "I’d meet you where the spirit meets the bone, in a faith forgotten land",
+    "And wonder about the only soul who can tell which smiles I'm fakin' \nAnd the heart I know I'm breakin' is my own\n  To leave the warmest bed I've ever known",
+    "You left me no choice but to stay here forever",
+  ],
+};
+
+const midnights = {
+  trackList: [
+    "Lavendar Haze",
+    "Maroon",
+    "Anti-Hero",
+    "Snow on the Beach",
+    "You're on You're Own, Kid",
+    "Midnight Rain",
+    "Vigilante Shit",
+    "Bejewelled",
+    "Labryinth",
+    "Question???",
+    "Karma",
+    "Sweet Nothing",
+    "Mastermind",
+    "The Great War",
+    "Bigger Than The Whole Sky",
+    "Paris",
+    "High Infidelity",
+    "Glitch",
+    "Would've, Could've, Should've",
+    "Dear Reader",
+  ],
+  released: "October 22, 2022",
+  quotes: [
+    "Hi, it's me! I'm the problem, it's me",
+    "Don’t put me in the basement when I want the penthouse of your heart",
+  ],
+};
+
+// Accessing id of albums from html img
+const albumInfo = document.getElementById("album-info");
+const startingGIF = document.getElementById("startingGIF");
+const debutOG = document.getElementById("debutOG");
+const TSTV = document.getElementById("TSTV");
+const fearlessOG = document.getElementById("fearlessOG");
+const fearlessTV = document.getElementById("fearlessTV");
+const speakNowOG = document.getElementById("SpeakNowOG");
+const speakNowTV = document.getElementById("SpeakNowTV");
+const redOG = document.getElementById("redOG");
+const redTV = document.getElementById("redTV");
+const nineteen89OG = document.getElementById("1989OG");
+const nineteen89TV = document.getElementById("1989TV");
+const reputationOG = document.getElementById("reputationOG");
+const reputationTV = document.getElementById("reputationTV");
+const loverImg = document.getElementById("lover");
+const folkloreImg = document.getElementById("folklore");
+const evermoreImg = document.getElementById("evermore");
+const midnightsImg = document.getElementById("midnights");
+const thumbsUpTaylor = document.getElementById("thumbs-up-taylor");
+
+// when an album img is clicked, an h3 and p element are created and populated with strings
+const buttonClicked = (album, OGvsTV, info) => {
+  albumInfo.replaceChildren();
+  const albumInfoH3 = albumInfo.appendChild(document.createElement("h3"));
+  const releasedInfo = albumInfo.appendChild(document.createElement("h4"));
+  const albumInfoP = albumInfo.appendChild(document.createElement("p"));
+  const quotes = albumInfo.appendChild(document.createElement("h4"));
+  albumInfoH3.textContent = album;
+  quotes.textContent = info.quotes;
+  albumInfoP.textContent = info.trackList;
+  if (OGvsTV === "OG") {
+    releasedInfo.textContent = info.released[0];
+  } else if (OGvsTV === "TV") {
+    releasedInfo.textContent = info.released[1];
+  } else if (OGvsTV === "TayOwned") {
+    releasedInfo.textContent = "Released on" + info.released;
+  } else {
+    console.log("OGvsTV tag is not working right");
+  }
+};
+
+debutOG.onclick = function () {
+  buttonClicked("Taylor Swift Debut", "OG", debut);
+};
+
+TSTV.onclick = function () {
+  buttonClicked("Taylor Swift TV", "TV", debut);
+};
+
+fearlessOG.onclick = function () {
+  buttonClicked("Fearless", "OG", fearless);
+};
+
+fearlessTV.onclick = function () {
+  buttonClicked("Fearless TV", "TV", fearless);
+};
+
+speakNowOG.onclick = function () {
+  buttonClicked("Speak Now", "OG", speakNow);
+};
+
+speakNowTV.onclick = function () {
+  buttonClicked("Speak Now TV", "TV", speakNow);
+};
+
+redOG.onclick = function () {
+  buttonClicked("Red", "OG", red);
+};
+
+redTV.onclick = function () {
+  buttonClicked("RED TV", "TV", red);
+};
+
+nineteen89OG.onclick = function () {
+  buttonClicked("1989", "OG", nineteen89);
+};
+
+nineteen89TV.onclick = function () {
+  buttonClicked("1989 TV", "TV", nineteen89);
+};
+
+reputation.onclick = function () {
+  buttonClicked("Reputation", "OG", reputation);
+};
+
+reputationTV.onclick = function () {
+  buttonClicked("Reputation TV", "TV", reputation);
+};
+
+loverImg.onclick = function () {
+  buttonClicked("Lover", "TayOwned", lover);
+};
+
+folkloreImg.onclick = function () {
+  buttonClicked("Folklore", "TayOwned", folklore);
+};
+
+evermoreImg.onclick = function () {
+  buttonClicked("Evermore", "TayOwned", evermore);
+};
+
+midnightsImg.onclick = function () {
+  buttonClicked("Midnights", "TayOwned", midnights);
+};
+
+thumbsUpTaylor.onclick = function () {
+  albumInfo.replaceChildren();
+  const albumInfoH3 = albumInfo.appendChild(document.createElement("h3"));
+  albumInfoH3.style.backgroundImage =
+    "url('/home/evergreen/TS_Tribute/assets/unicornCat.gif')";
+};
+
+// // when debutOG img is clicked, an h3 element is created
+// debutOG.onclick = function () {
+//   startingGIF.style.display = "none";
+//   const albumInfo = document.getElementById("album-info");
+//   const albumInfoH3 = albumInfo.appendChild(document.createElement("h3"));
+//   const albumInfoP = albumInfo.appendChild(document.createElement("p"))
+//   albumInfoH3.textContent = "AlbumName!";
+//   albumInfoP.textContent = "Track names, producer, year released, favorite lyrics"
+// };
+
+// const paragraph = document.body.appendChild(document.createElement("p"));
